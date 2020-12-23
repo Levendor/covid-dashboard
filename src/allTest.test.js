8548fa06c5680d70efa0971aaabbd948f272b5a5
Tav25 <turbin28@gmail.com>
2020-12-23 05:44:07 +0300

    feat: add tests

---
+++ src/allTest.test.js
@@ -0,0 +1,36 @@
+
+import Table from './Table-view';
+it('it is possible to call new Table()', () => {
+  const testTable = new Table();
+  expect(testTable).toBeTruthy();
+});
+
+import ViewDate from './Date-view';
+it('it is possible to call new ViewDate()', () => {
+  const testDate = new ViewDate();
+  expect(testDate).toBeTruthy();
+});
+
+import List from './List-view';
+it('it is possible to call new List()', () => {
+  const testList = new List();
+  expect(testList).toBeTruthy();
+});
+
+import Observer from './Observer';
+it('it is possible to call new Observer()', () => {
+  const testObserver = new Observer();
+  expect(testObserver).toBeTruthy();
+});
+
+import Search from './Search-view';
+it('it is possible to call new Search()', () => {
+  const testSearch = new Search();
+  expect(testSearch).toBeTruthy();
+});
+
+
+
+
+
+
